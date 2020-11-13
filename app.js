ece3f68550b5ce05c5e45bb9ce396746cf514279
edwin001-tech <edwinshitakwa8@gmail.com>
2020-11-13 12:12:38 +0300

    my very first template

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-    // res.write("<h1>Hello this is localhost</h1>");
-    // res.write("<h1>Hello this is localhost</h1>");
-    // res.write("<p>Hello this is localhost</p>");
 
-    res.sendFile(__dirname +"/index.html");
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+    
+    switch(currentDay){
+        case 0:
+            day = "Sunday";
+            case 1:
+            day = "Monday";
+            case 2:
+            day = "Tuesday";
+            case 3:
+            day = "Wednesday";
+            case 4:
+            day = "Thursday";
+            case 5:
+            day = "Friday";
+            case 6:
+            day = "Saturday";
+            break;
+            default:
+                console.log("Error: Current day is "+ currentDay);
+
+            
+            
+    } 
+
+    res.render("list", {KindOfDay: day});
+    
 });
 
 app.listen(3000, function(){
